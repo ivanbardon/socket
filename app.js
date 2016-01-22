b18b78542b3c454bec2e3bf3667708f2a9ba4361
Ivan <elsegundopar@gmail.com>
2016-01-22 04:01:15 +0100

    get traction

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
+app.set('view engine','jade');
+
+app.get('/chat', function(req, res){
   res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/', function(req, res){
+	res.render('chat');
 });
 
 io.on('connection', function(socket){
@@ -13,5 +19,5 @@ io.on('connection', function(socket){
 });
 
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+  console.log('listening on port: 3000');
 });
