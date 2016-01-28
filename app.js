15f9253060c4b587e581c617fb8aad32d3d964f1
Ivan <elsegundopar@gmail.com>
2016-01-28 22:33:42 +0100

    add comments to app.js

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+//Require de los modulos
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+//Configuracion
 app.set('view engine','jade');
+app.set('port', process.env.PORT || 3000);
 
+//Rutas
 app.get('/fw/:name', function(req, res){
 	res.render('fw');
 });
@@ -16,6 +20,7 @@ app.get('/chat', function(req, res){
 	res.render('chat');
 });
 
+//Manejo de eventos en los clientes
 io.on('connection', function(socket){
 	socket.on('send loc', function(data){
 		io.emit('print loc', data)
@@ -24,8 +29,8 @@ io.on('connection', function(socket){
 		io.emit('chat message', msg);
   });
 });
-app.set('port', process.env.PORT || 3000);
 
+//Puerto para servir
 http.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
